b5c10b5bd67add8c4c897805f4bdf256d622bb41
Jean Brito <jeanfbrito@gmail.com>
2012-10-25 00:53:12 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackMyWip::Application.config.secret_token = 'b19876c28ffbe6029a6dc4dcc9d41cb978199a3d0a4acb787c4559b76d9330cc62bfcd3c3a04654442806593976da95d93af62220c0f5162397fc769f0b5df44'
